6a24b46d50e9ecb0149e41d953ae979d2e4a8dd3
kenny-nguyen <developedbykenny@gmail.com>
2025-01-22 16:25:51 +0700

    add client file

---
+++ src/_data/client.js
@@ -0,0 +1,23 @@
+export const client = {
+	name: 'Business Name',
+	email: 'name@domain.com',
+	phoneForTel: '555-555-555',
+	address: {
+		lineOne: 'First Address Line',
+		lineTwo: 'Second Address Line',
+		city: 'City',
+		province: 'Province.',
+		postcalCode: 'A1A 1A1',
+		country: 'CountryCode',
+		mapLink: 'https://maps.app.goo.gl/yhMP9U2iPeaXMPdj9',
+	},
+	socials: {
+		facebook:
+			'https://www.facebook.com/people/Henderson-Home-Improvements/100091092607361/?_rdr',
+	},
+	links: {
+		logo: 'https://i.imgur.com/uRlzXA9.png',
+	},
+	//! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+	domain: 'https://www.hendersonhomeimprovements.ca',
+};
